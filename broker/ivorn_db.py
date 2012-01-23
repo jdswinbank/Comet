3492b7d1f9eaa9fb96bab394f42a0eb0a715cffa
John Swinbank <swinbank@transientskp.org>
2012-01-23 18:23:25 +0100

    Lock ivorn database when in use.
    Hopefully avoid issues with multiple receiver threads interlocking.

--- broker/ivorn_db.py
+++ broker/ivorn_db.py
@@ -6,17 +6,24 @@ import os
 import anydbm
 import datetime
 from contextlib import closing
+from threading import Lock
 
 class IVORN_DB(object):
+    # Using one big lock for all the databases is a little clunky.
     def __init__(self, root):
         self.root = root
+        self.lock = Lock()
 
     def check_ivorn(self, ivorn):
         db_path, key = ivorn.split('//')[1].split('#')
         db_path = db_path.replace(os.path.sep, "_")
-        with closing(anydbm.open(os.path.join(self.root, db_path), 'c')) as db:
+        try:
+            self.lock.acquire()
+            db = anydbm.open(os.path.join(self.root, db_path), 'c')
             if db.has_key(key):
                 return False # Should not forward
             else:
                 db[key] = str(datetime.datetime.utcnow())
                 return True # Ok to forward
+        finally:
+            self.lock.release()
