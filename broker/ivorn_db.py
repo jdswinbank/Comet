e43c1335bb48e8ba3321ddd9471ac04fd75a4250
John Swinbank <swinbank@transientskp.org>
2012-01-24 08:57:28 +0100

    Use a separate lock per ivorn database

--- broker/ivorn_db.py
+++ broker/ivorn_db.py
@@ -5,20 +5,19 @@
 import os
 import anydbm
 import datetime
-from contextlib import closing
 from threading import Lock
+from collections import defaultdict
 
 class IVORN_DB(object):
-    # Using one big lock for all the databases is a little clunky.
     def __init__(self, root):
         self.root = root
-        self.lock = Lock()
+        self.locks = defaultdict(Lock)
 
     def check_ivorn(self, ivorn):
         db_path, key = ivorn.split('//')[1].split('#')
         db_path = db_path.replace(os.path.sep, "_")
         try:
-            self.lock.acquire()
+            self.locks[db_path].acquire()
             db = anydbm.open(os.path.join(self.root, db_path), 'c')
             if db.has_key(key):
                 return False # Should not forward
@@ -26,4 +25,4 @@ class IVORN_DB(object):
                 db[key] = str(datetime.datetime.utcnow())
                 return True # Ok to forward
         finally:
-            self.lock.release()
+            self.locks[db_path].release()
