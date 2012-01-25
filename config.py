e60b9da51ae2e5ba093d75b253d010b0c49e0bad
John Swinbank <swinbank@transientskp.org>
2012-01-25 18:16:09 +0100

    Switch to 'standard' ports

--- config.py
+++ config.py
@@ -2,11 +2,11 @@
 # John Swinbank, <swinbank@transientskp.org>, 2012.
 
 # This is quick and dirty!
-RECEIVER_LISTEN_ON    = "tcp:8099"
-PUBLISHER_LISTEN_ON   = "tcp:8100"
-SENDER_CONNECT_TO     = "tcp:host=localhost:port=8099"
+RECEIVER_LISTEN_ON    = "tcp:8098"
+PUBLISHER_LISTEN_ON   = "tcp:8099"
+SENDER_CONNECT_TO     = "tcp:host=localhost:port=8098"
 SUBSCRIBER_HOST       = "localhost"
-SUBSCRIBER_PORT       = 8100
+SUBSCRIBER_PORT       = 8099
 BROKER_SUBSCRIBE_TO   = [("voevent.dc3.com", 8099)]
 LOCAL_IVO             = "ivo://lofar/transients"
 IVORN_DB_ROOT         = "/tmp"
