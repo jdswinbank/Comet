517c638e68a62e1413a1c106defc866d01852650
John Swinbank <swinbank@transientskp.org>
2012-01-27 00:43:23 +0100

    Add basic distutils setup script

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+import distutils.sysconfig
+from distutils.core import setup
+
+setup(
+    name="Comet",
+    description="VOEvent Broker",
+    author="John Swinbank",
+    author_email="swinbank@transientskp.org",
+    packages=[
+        'comet',
+        'comet.config',
+        'comet.service',
+        'comet.tcp',
+        'comet.utility',
+        'comet.voevent'
+    ],
+    scripts=['scripts/send_voevent'],
+    data_files=[
+        (
+            os.path.join(
+                distutils.sysconfig.get_python_lib(prefix=''),
+                'twisted/plugins'
+            ),
+            [
+                'twisted/plugins/broker_plugin.py',
+                'twisted/plugins/subscriber_plugin.py'
+            ]
+        )
+    ]
+)
