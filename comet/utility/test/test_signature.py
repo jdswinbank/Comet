e64e1d214eccee5101de5ac9b5871d2b880dd1a1
John Swinbank <swinbank@transientskp.org>
2012-08-25 20:34:44 +0200

    More tests for CheckSignature

--- comet/utility/test/test_signature.py
+++ comet/utility/test/test_signature.py
@@ -2,9 +2,13 @@ import os
 
 from twisted.trial import unittest
 
-import comet
 from ...icomet import IValidator
 from ..signature import CheckSignature
+from ..xml import xml_document
+
+from ...test.support import DUMMY_VOEVENT
+from ...test.gpg import GPGTestSupport
+from ...tcp.test.test_voeventreceiver import VOEventReceiverTestCaseBase
 
 class ValidEvent(object):
     def valid_signature(self):
@@ -26,3 +30,37 @@ class CheckSignatureTestCase(unittest.TestCase):
 
     def test_interface(self):
         self.assertTrue(IValidator.providedBy(self.validator))
+
+class CheckSignatureValidatorTestCase(VOEventReceiverTestCaseBase, GPGTestSupport):
+    def setUp(self):
+        VOEventReceiverTestCaseBase.setUp(self)
+        GPGTestSupport.setUp(self)
+
+    def test_receive_voevent_unsigned(self):
+        # Unsigned events should be rejected
+        self.factory.validators = [CheckSignature()]
+        self.tr.clear()
+        d = self.proto.stringReceived(DUMMY_VOEVENT)
+        d.addCallback(self._sent_nak)
+        d.addCallback(self._transport_disconnected)
+        return d
+
+    def test_receive_voevent_signed_untrusted(self):
+        # Event with untrusted sig should be rejected
+        self.factory.validators = [CheckSignature()]
+        doc = xml_document(DUMMY_VOEVENT)
+        doc = self._sign_untrusted(doc)
+        d = self.proto.stringReceived(doc.text)
+        d.addCallback(self._sent_nak)
+        d.addCallback(self._transport_disconnected)
+        return d
+
+    def test_receive_voevent_signed_trusted(self):
+        # Event with trusted sig should be accepted
+        self.factory.validators = [CheckSignature()]
+        doc = xml_document(DUMMY_VOEVENT)
+        doc = self._sign_trusted(doc)
+        d = self.proto.stringReceived(doc.text)
+        d.addCallback(self._sent_ack)
+        d.addCallback(self._transport_disconnected)
+        return d
