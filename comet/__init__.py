0270757624cb2a8d01f22cd3049edc4f1602e4e5
John Swinbank <swinbank@transientskp.org>
2012-08-29 13:02:39 +0200

    don't mistakenly set module name

--- comet/__init__.py
+++ comet/__init__.py
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 __description__ = "VOEvent Broker"
 __url__ = "http://comet.transientskp.org/"
 __author__ = "John Swinbank"
 __contact__ = "j.swinbank@uva.nl"
-=======
-__name__ = "Comet"
-__description__ = "VOEvent Broker"
-__url__ = "http://comet.transientskp.org/"
-__author__ = "John Swinbank"
-__contact__ = "swinbank@transientskp.org"
->>>>>>> Embed version information into __init__.py
 __version__ = "1.1.0-pre"
