3cf3831b6c21b60c0077d253a64cf634807edeba
John Swinbank <swinbank@transientskp.org>
2012-09-05 11:58:02 +0200

    update text on change to element & vice versa

--- comet/utility/test/test_xml.py
+++ comet/utility/test/test_xml.py
@@ -5,6 +5,21 @@ from comet.utility.xml import xml_document
 
 EXAMPLE_XML = """<xml></xml>"""
 
+class mutable_element_tests(unittest.TestCase):
+    def setUp(self):
+        self.doc = xml_document("<foo>bar</foo>")
+
+    def test_transform_text(self):
+        self.assertEqual(self.doc.element.text, "bar")
+        self.doc.text = "<foo>baz</foo>"
+        self.assertEqual(self.doc.element.text, "baz")
+
+    def test_transform_element(self):
+        self.assertEqual(self.doc.text.find("<foo>baz</foo>"), -1)
+        self.doc.element = ElementTree.fromstring("<foo>baz</foo>")
+        self.assertNotEqual(self.doc.text.find("<foo>baz</foo>"), -1)
+
+
 class xml_document_tests(object):
     def test_signature(self):
         self.assertFalse(self.doc.valid_signature)
--- comet/utility/xml.py
+++ comet/utility/xml.py
@@ -1,20 +1,32 @@
 import lxml.etree as ElementTree
 
 class xml_document(object):
-    __slots__ = ["element", "text"]
+    __slots__ = ["_element", "_text"]
 
     def __init__(self, document):
         if isinstance(document, ElementTree._Element):
             self.element = document
-            self.text =  ElementTree.tostring(
-                self.element,
-                xml_declaration=True,
-                encoding="UTF-8",
-                pretty_print=True
-            )
         else:
             self.text = document
-            self.element = ElementTree.fromstring(self.text)
+
+    def get_text(self):
+        return self._text
+    def set_text(self, value):
+        self._text = value
+        self._element = ElementTree.fromstring(self._text)
+    text = property(get_text, set_text)
+
+    def get_element(self):
+        return self._element
+    def set_element(self, value):
+        self._element = value
+        self._text =  ElementTree.tostring(
+            self._element,
+            xml_declaration=True,
+            encoding="UTF-8",
+            pretty_print=True
+        )
+    element = property(get_element, set_element)
 
     @property
     def valid_signature(self):
