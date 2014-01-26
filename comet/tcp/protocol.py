2ac9806f01d3e41651a94ad8cbe8dba95afa3908
John Swinbank <j.swinbank@uva.nl>
2014-01-26 15:21:40 +0100

    Broadcasting test events is configurable

--- comet/tcp/protocol.py
+++ comet/tcp/protocol.py
@@ -33,6 +33,8 @@ from ..utility.auth import check_auth, CheckSignatureMixin
 
 # Constants
 VOEVENT_ROLES = ('observation', 'prediction', 'utility', 'test')
+# By default, we brodcast a test event every BCAST_TEST_INTERVAL seconds.
+BCAST_TEST_INTERVAL = 3600
 
 """
 Implements the VOEvent Transport Protocol; see
