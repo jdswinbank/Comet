49549168c206ccb6172817a0b8be403413c1b6d4
John Swinbank <j.swinbank@uva.nl>
2014-01-26 16:09:18 +0100

    Update package metadata

--- comet/__init__.py
+++ comet/__init__.py
@@ -1,5 +1,5 @@
 __description__ = "VOEvent Broker"
 __url__ = "http://comet.transientskp.org/"
 __author__ = "John Swinbank"
-__contact__ = "swinbank@transientskp.org"
+__contact__ = "j.swinbank@uva.nl"
 __version__ = "1.1.0-pre"
