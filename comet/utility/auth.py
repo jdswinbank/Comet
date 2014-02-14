0ff8adb6f95199a6b37c3d357d44742e5e051a0b
John Swinbank <j.swinbank@uva.nl>
2014-02-14 17:30:50 +0100

    Update copyright date

--- comet/utility/auth.py
+++ comet/utility/auth.py
@@ -1,6 +1,6 @@
 # Comet VOEvent Broker.
 # Authentication routines.
-# John Swinbank, <swinbank@transientskp.org>, 2012.
+# John Swinbank, <swinbank@transientskp.org>.
 
 from functools import wraps
 from twisted.internet.threads import deferToThread
--- comet/validator/signature.py
+++ comet/validator/signature.py
@@ -1,6 +1,6 @@
 # Comet VOEvent Broker.
 # Check OpenPGP signatures.
-# John Swinbank, <swinbank@transientskp.org>, 2012.
+# John Swinbank, <swinbank@transientskp.org>.
 
 from zope.interface import implementer
 
