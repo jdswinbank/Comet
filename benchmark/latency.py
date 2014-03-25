a1196d02aaba80cd3b16d6d45ae908a19850f20f
John Swinbank <j.swinbank@uva.nl>
2014-03-25 17:05:16 +0100

    Log event latency to disk

--- benchmark/latency.py
+++ benchmark/latency.py
@@ -4,49 +4,44 @@
 # a timestamp for the time the event was produced.
 # John Swinbank, <swinbank@trtransientskp.org>.
 
+import os
 import datetime
+import numpy
 import lxml.etree as ElementTree
 from zope.interface import implementer
 from twisted.plugin import IPlugin
-from ..icomet import IHandler
+from ..icomet import IHandler, IHasOptions
 from ..utility import log
 
-# Event handlers must implement IPlugin and IHandler.
-@implementer(IPlugin, IHandler)
+@implementer(IPlugin, IHandler, IHasOptions)
 class LatencyMeasure(object):
-    # Simple example of an event handler plugin. This simply prints the
-    # received event to standard output.
-
-    # The name attribute enables the user to specify plugins they want on the
-    # command line.
     name = "latency"
 
     def __init__(self):
-        self.n_events = 0
-        self.max_time = None
-        self.min_time = None
-        self.mean_time = None
+        self.latencies = []
+        self.output = os.path.join(os.getcwd(), "latencies.log")
 
     # When the handler is called, it is passed an instance of
     # comet.utility.xml.xml_document.
     def __call__(self, event):
-        """
-        Print an event to standard output.
-        """
         incoming_date = datetime.datetime.strptime(
             event.find("Who").find("Date").text,
             "%Y-%m-%dT%H:%M:%S.%f"
         )
         td = datetime.datetime.now() - incoming_date
-        if not self.n_events:
-            self.n_events = 1
-            self.max_time = self.min_time = self.mean_time = td.total_seconds()
-        else:
-            self.max_time = max(td.total_seconds(), self.max_time)
-            self.min_time = min(td.total_seconds(), self.min_time)
-            self.mean_time = (self.mean_time * self.n_events + td.total_seconds()) / (self.n_events + 1)
-            self.n_events += 1
-        log.info("Received %d events; latency min/max/mean: %f/%f/%f s" % (self.n_events, self.min_time, self.max_time, self.mean_time))
+        self.latencies.append(td.total_seconds())
+        # Write to disk every 10 events
+        if not len(self.latencies) % 10:
+            with open(self.output, 'a') as output_file:
+                output_file.writelines(str(x) + "\n" for x in self.latencies)
+                self.latencies = []
+
+    def get_options(self):
+        return [('output', self.output, 'Log file')]
+
+    def set_option(self, name, value):
+        if name == "output":
+            self.output = value
 
 # This instance of the handler is what actually constitutes our plugin.
 latency = LatencyMeasure()
