c64ad771013c1176f3b639c81af6651b3a5bd901
Tim Staley <git@timstaley.co.uk>
2016-03-15 09:15:04 +0000

    Bugfix: Fixup a few missed references to 'log.warning'.
    These have been replaced by log.warn, the 'warning' alias has been removed
    and now results in an AttributeError.
    
    This was causing comet-sendvo to hang rather than exit with an error
    code if e.g. no broker-connection can be established.

--- comet/service/broker.py
+++ comet/service/broker.py
@@ -195,6 +195,6 @@ def makeService(config):
         remote_service.setServiceParent(broker_service)
 
     if not broker_service.services:
-        reactor.callWhenRunning(log.warning, "No services requested; stopping.")
+        reactor.callWhenRunning(log.warn, "No services requested; stopping.")
         reactor.callWhenRunning(reactor.stop)
     return broker_service
