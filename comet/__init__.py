0aee34bc19d43f2369a121da2f9cfff05225fdbc
John Swinbank <swinbank@princeton.edu>
2016-04-03 13:32:09 -0400

    Add alias to appropriate raw bytes for this Python.

--- comet/__init__.py
+++ comet/__init__.py
@@ -3,3 +3,10 @@ __url__ = "http://comet.transientskp.org/"
 __author__ = "John Swinbank"
 __contact__ = "swinbank@princeton.edu"
 __version__ = "2.1.0-pre"
+
+import sys
+
+if sys.version_info.major <= 2:
+    BINARY_TYPE = str
+else:
+    BINARY_TYPE = bytes
