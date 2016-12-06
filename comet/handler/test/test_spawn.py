a07e87f6df66ea022152f7c103cb618954e795d0
John Swinbank <swinbank@princeton.edu>
2016-12-06 11:16:48 -0500

    Test for logging of cmd return value & stdout.

--- comet/handler/test/test_spawn.py
+++ comet/handler/test/test_spawn.py
@@ -5,10 +5,12 @@ from unittest import skipUnless
 
 from twisted.trial import unittest
 from twisted.python import util
+from twisted.python import log as twisted_log
 
+import comet.log as log
 from comet.icomet import IHandler
 from comet.handler import SpawnCommand
-from comet.testutils import DummyEvent
+from comet.testutils import DummyEvent, DummyLogObserver
 
 # Used for spawning test code.
 SHELL = '/bin/sh'
@@ -29,12 +31,49 @@ class SpawnCommandProtocolTestCase(unittest.TestCase):
         d.addCallback(self.assertEqual, True)
         return d
 
-    def test_bad_process(self):
+    def test_no_exec(self):
+        """
+        Fail when the executable doesn't exist.
+        """
         spawn = SpawnCommand("/not/a/real/executable")
         return self.assertFailure(spawn(DummyEvent()), Exception)
 
+    def _check_logged_value(self, script_name, sentinel, should_fail=False):
+        observer = DummyLogObserver()
+        twisted_log.addObserver(observer)
+        log.LEVEL = log.Levels.DEBUG
+        spawn = SpawnCommand(SHELL, util.sibpath(__file__, script_name))
+        def _check_log(_):
+            self.assertTrue(sentinel in observer.messages[1][0])
+        d = spawn(DummyEvent())
+        if should_fail:
+            return self.assertFailure(d, Exception).addCallback(_check_log)
+        else:
+            d.addCallback(_check_log)
+            return d
+
+    @skipUnless(os.access(SHELL, os.X_OK), "Shell executable not available")
+    def test_bad_process(self):
+        """
+        Fail when the executable returns non-zero
+        """
+        # Script returns a 99; we should be able to read that in the logs.
+        return self._check_logged_value("test_spawn_failure.sh", "99", True)
+
+    @skipUnless(os.access(SHELL, os.X_OK), "Shell executable not available")
+    def test_stdout(self):
+        """
+        Demonstrate we can read stdout from the process.
+        """
+        # Script prints the string TESTTEST; we should be able to read that in
+        # the logs.
+        return self._check_logged_value("test_spawn_stdout.sh", "TESTTEST")
+
     @skipUnless(os.access(SHELL, os.X_OK), "Shell executable not available")
     def test_write_data(self):
+        """
+        Demonstrate that an external command can save data.
+        """
         output_file = tempfile.NamedTemporaryFile()
         dummy_event = DummyEvent()
         def read_data(result):
@@ -44,7 +83,7 @@ class SpawnCommandProtocolTestCase(unittest.TestCase):
                 self.assertEqual(output_file.read(), dummy_event.raw_bytes)
             finally:
                 output_file.close()
-        spawn = SpawnCommand(SHELL, util.sibpath(__file__, "test_spawn.sh"), output_file.name)
+        spawn = SpawnCommand(SHELL, util.sibpath(__file__, "test_spawn_output.sh"), output_file.name)
         d = spawn(dummy_event)
         d.addCallback(read_data)
         return d
