5e2eb5e205c709adf1e22143aaebf8e77e66a6ff
John Swinbank <swinbank@princeton.edu>
2016-12-06 10:12:59 -0500

    Log return codes & stdout from external processes.

--- comet/handler/spawn.py
+++ comet/handler/spawn.py
@@ -14,24 +14,29 @@ import comet.log as log
 __all__ = ["SpawnCommand"]
 
 class SpawnCommandProtocol(ProcessProtocol):
+    # Assume that all external processes write a UTF-8 bytestream to STDOUT.
+    # This is obviously a questionable assumption, but it's not clear what a
+    # better alternative would be (probably trying to auto-detect, but that's
+    # error prone).
+    STDOUT_ENCODING = "UTF-8"
+
     def __init__(self, deferred, raw_bytes):
         self.deferred = deferred
         self.raw_bytes = raw_bytes
 
     def connectionMade(self):
         # Note that we're squiring whatever encoding raw_bytes happens to be
-        # in at the process. An alternative option would be to normalize it to
-        # UTF-8. Right answer probably depends on the recipient.
+        # in at the process.
         self.transport.write(self.raw_bytes)
         self.transport.closeStdin()
 
+    def outReceived(self, data):
+        log.debug("External process said: %s" %
+                  (data.decode(self.STDOUT_ENCODING),))
+
     def processEnded(self, reason):
         if reason.value.exitCode:
-            self.deferred.errback(
-                Exception(
-                    "Process returned non-zero (%d)" % (reason.value.exitCode,)
-                )
-            )
+            self.deferred.errback(reason)
         else: self.deferred.callback(True)
 
 @implementer(IHandler)
@@ -55,4 +60,17 @@ class SpawnCommand(object):
             args=self.args,
             env=os.environ
         )
+        def log_reason(reason):
+            """
+            Catch a Failure returned from an unsuccessful process execution
+            and log the return value, then re-raise the error.
+            """
+            if not os.access(self.cmd, os.X_OK):
+                msg = "%s is not an executable" % (self.cmd,)
+            else:
+                msg = "%s returned non-zero (%d)" % (self.cmd,
+                                                     reason.value.exitCode)
+            log.warn(msg)
+            return reason
+        d.addErrback(log_reason)
         return d
