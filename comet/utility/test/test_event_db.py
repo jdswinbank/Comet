0203e22790f5c8340278366cb85093aa1b305623
John Swinbank <john@swinbank.org>
2019-01-05 20:18:16 +0000

    Disambiguate test database filenames.
    On Windows, the successive calls to time.time() can return the same value, and
    hence cause collisions unless we take other steps to ensure the filenames are
    different.

--- comet/utility/test/test_event_db.py
+++ comet/utility/test/test_event_db.py
@@ -28,13 +28,13 @@ class Event_DB_TestCase(unittest.TestCase):
         # If the root for the Event_DB doesn't exist, we should create it.
         # Note the relative path means that this DB will be created in the
         # _trial_temp directory.
-        event_db = Event_DB("event_db_test_%.5f" % (time.time(),))
+        event_db = Event_DB("event_db_test_non_exist_%.5f" % (time.time(),))
         self.assertTrue(event_db.check_event(self.event))
 
     def test_dir_is_file(self):
         # If the path specified for the Event_DB *does* exist but isn't a
         # directory, then we should fail fast.
-        filename = "event_db_test_%.5f" % (time.time(),)
+        filename = "event_db_test_is_file_%.5f" % (time.time(),)
         open(filename, 'w').close()
         self.assertRaises(RuntimeError, Event_DB, filename)
 
@@ -43,7 +43,7 @@ class Event_DB_TestCase(unittest.TestCase):
         # If the path specified for the Event_DB exists and is a directory,
         # but we don't have permissions to use it, fail fast.
         # Note that os.chmod() isn't properly supported on Windows.
-        filename = "event_db_test_%.5f" % (time.time(),)
+        filename = "event_db_test__is_unusable_%.5f" % (time.time(),)
         os.makedirs(filename)
         os.chmod(filename, 0)
         self.assertRaises(RuntimeError, Event_DB, filename)
